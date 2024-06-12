25521e9df87378d29f1483558e65c43b764a6e18
Gabriel Vital <gabrit_8@hotmail.com>
2024-06-12 12:03:18 -0300

    Gerando NIS no Model e Readme atualizado

--- app/Controller/ControllerCidadao.php
+++ app/Controller/ControllerCidadao.php
@@ -18,11 +18,8 @@ switch ($acao) {
     case 'adicionar':
         adicionarCidadao();
         break;
-    case 'editar':
-        editarCidadao();
-        break;
-    case 'excluir':
-        excluirCidadao();
+    case 'pesquisar':
+        pesquisarCidadao();
         break;
 }
 
@@ -39,42 +36,15 @@ function adicionarCidadao() {
 
     //campos a serem recebidos da view
     $nome = $_POST['nome'];
-    $nis = $_POST['nis'];
 
     //adiciono os elementos em um objeto Cidadao
     $cidadao->setNome($nome);
-    $cidadao->setNis($nis);
 
     //chamo a função da DAO, que efetivamente manipula o BD
     $dao->adicionarCidadao($cidadao);
 }
 
-function editarCidadao() {
-    //inclui o modelo, a dao e a database
-    require_once ('../Model/ModelCidadao.php');
-    require_once ('../Dao/DaoCidadao.php');
-    require_once('../Database/Database.php');
-
-    //instancia das classes
-    $db = new Database();
-    $dao = new DaoCidadao();
-    $cidadao = new ModelCidadao();
-
-    //campos a serem recebidos da view
-    $id = $_POST['id'];
-    $nome = $_POST['nome'];
-    $nis = $_POST['nis'];
-
-    //adiciono os elementos em um objeto Cidadao
-    $cidadao->setId($id);
-    $cidadao->setNome($nome);
-    $cidadao->setNis($nis);
-
-    //chamo a função da DAO, que efetivamente manipula o BD
-    $dao->editarCidadao($cidadao);
-}
-
-function excluirCidadao() {
+function pesquisarCidadao() {
     //inclui o modelo, a dao e a database
     require_once ('../Model/ModelCidadao.php');
     require_once ('../Dao/DaoCidadao.php');
@@ -86,11 +56,18 @@ function excluirCidadao() {
     $cidadao = new ModelCidadao();
 
     //id do usuário a ser deletado
-    $id = $_POST['id'];
+    $nis = $_POST['nis'];
 
     $cidadao = new ModelCidadao();
-    $cidadao->setId($id);
+    $cidadao->setNis($nis);
     
-    //chamo a função da DAO, que efetivamente manipula o BD
-    $dao->excluirCidadao($cidadao);
+    //função da DAO, que efetivamente manipula o BD
+    $result = $dao->pesquisarCidadao($cidadao);
+
+    if ($result) {
+        echo json_encode(['status' => 'success', 'data' => $result]);
+    } else {
+        echo json_encode(['status' => 'error', 'message' => 'Usuário não encontrado']);
+    }
+    
 }
--- app/Dao/DaoCidadao.php
+++ app/Dao/DaoCidadao.php
@@ -55,52 +55,24 @@ class DaoCidadao {
         }
     }
 
-    public function editarCidadao(ModelCidadao $cidadao) {
+    public function pesquisarCidadao(ModelCidadao $cidadao) {
         try {
             //recupero valores do Model
-            $id = $cidadao->getId();
-            $nome = $cidadao->getNome();
             $nis = $cidadao->getNis();
 
             //preparo o comando
-            $stmt = $this->conn->prepare("UPDATE Cidadao SET nomeCidadao = ?, nisCidadao = ? WHERE idCidadao = ?");
+            $stmt = $this->conn->prepare("SELECT * FROM Cidadao WHERE nisCidadao = ?");
 
             //bind nos valores dos campos
-            $stmt->bindparam(1, $nome);
-            $stmt->bindparam(2, $nis);
-            $stmt->bindparam(3, $id);
-
-            //executo a instrução
+            $stmt->bindparam(1, $nis);
             $stmt->execute();
 
             //verifico se deu certo
             if ($stmt->rowCount() > 0) {
-                echo 1;
+                $result = $stmt->fetch(PDO::FETCH_ASSOC);
+                return $result;
             } else {
-                echo 2;
-            }
-        } catch (PDOException $e) {
-            echo $e->getMessage();
-        }
-    }
-
-    public function excluirCidadao(ModelCidadao $cidadao) {
-        try {
-            //recupero valores do Model
-            $id = $cidadao->getId();
-
-            //preparo o comando
-            $stmt = $this->conn->prepare("DELETE FROM Cidadao WHERE idCidadao = ?");
-
-            //bind nos valores dos campos
-            $stmt->bindparam(1, $id);
-            $stmt->execute();
-
-            //verifico se deu certo
-            if ($stmt->rowCount() > 0) {
-                echo 1;
-            } else {
-                echo 2;
+                return null;
             }
         } catch (PDOException $e) {
             echo $e->getMessage();
