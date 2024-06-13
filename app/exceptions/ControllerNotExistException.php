9171a0ae81927f5d1bf67a01074cc6c9c9b278b1
Gabriel Vital <gabrit_8@hotmail.com>
2024-06-12 22:28:15 -0300

    começei fazer o controller da api

---
+++ app/exceptions/ControllerNotExistException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace app\exceptions;
+
+
+class ControllerNotExistException extends \Exception {
+    
+}
+
+?>
---
+++ public/index.php
@@ -0,0 +1,21 @@
+<?php
+
+
+
+require '../bootstrap.php';
+
+use core\Controller;
+
+
+try {
+    
+    $controller = new Controller;
+    $controller = $controller->load();
+    dd($controller);
+
+}catch (\Exception $e){
+    dd($e->getMessage());
+}
+
+
+?>
